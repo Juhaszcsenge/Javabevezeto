11886cd1d8a312868e4d83dd1ff51a8dfb09ef91
Juhász Csenge <csenge799@gmail.com>
2022-09-15 17:29:22 +0200

    Kérjen egy Stringet a felhasználótól.

---
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -0,0 +1,12 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Scanner;
+
+public class Feladat14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adjon meg egy szöveget: ");
+        String szoveg = sc.nextLine();
+
+    }
+}
